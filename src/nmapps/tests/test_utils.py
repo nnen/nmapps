acc2c9bc4fff45367966b3f4c5341c70116e9449
Jan Milík <milikjan@fit.cvut.cz>
2013-07-21 17:02:54 +0200

    Add unit tests for .

--- src/nmapps/tests/test_utils.py
+++ src/nmapps/tests/test_utils.py
@@ -5,6 +5,22 @@ import unittest
 import nmapps.utils as utils
 
 
+class TestArgsRepr(unittest.TestCase):
+    def test_no_arguments(self):
+        self.assertEqual(utils.args_repr(), "")
+    
+    def test_positional(self):
+        self.assertEqual(utils.args_repr(1, 2, 3), "1, 2, 3")
+        self.assertEqual(utils.args_repr(1, 2, 3, "abc"), "1, 2, 3, 'abc'")
+    
+    def test_keywords(self):
+        self.assertEqual(utils.args_repr(kw1 = 1, kw2 = 2), "kw1 = 1, kw2 = 2")
+    
+    def test_both(self):
+        self.assertEqual(utils.args_repr(1, 2, 3, "abc", kw1 = 1, kw2 = 2),
+                         "1, 2, 3, 'abc', kw1 = 1, kw2 = 2")
+
+
 class TestPrefixDict(unittest.TestCase):
     def test_setitem(self):
         prefd = utils.PrefixDict()
@@ -17,7 +33,7 @@ class TestPrefixDict(unittest.TestCase):
         prefd["hello"] = 0
         prefd["hell"]  = 1
         prefd["abc"]   = 2
-
+        
         self.assertEqual(1, len(prefd["hello"]))
         self.assertEqual(2, len(prefd["hell"]))
         self.assertEqual(2, len(prefd["hel"]))
@@ -51,5 +67,80 @@ class TestUserException(unittest.TestCase):
             self.assertIsNotNone(e.inner_exception)
             self.assertIsNotNone(e.inner_exception.exc_info)
             self.assertIsNotNone(e.inner_exception.exc_info[2])
+
+
+class TestNull(unittest.TestCase):
+    def setUp(self):
+        self.null_obj = utils.Null()
+    
+    def test_constructor(self):
+        n = utils.Null()
+        n = utils.Null("value")
+        n = utils.Null("arg1", "arg2", "arg3")
+        n = utils.Null("arg1", "arg2", "arg3", kw_arg0 = 0, kw_arg1 = 1)
+    
+    def test_calling(self):
+        self.null_obj()
+        self.null_obj("arg")
+        self.null_obj("arg0", "arg1")
+        self.null_obj("arg0", "arg1", arg0 = "something")
+
+    def test_attributes(self):
+        self.null_obj.attr1
+        self.null_obj.attr2
+        
+        self.null_obj.attr1.attr2.attr3
+        
+        self.null_obj.method()
+        self.null_obj.method("value")
+        self.null_obj.method1().method2()
+        self.null_obj.method1("value").method2(1, 2, 3)
+        
+        self.null_obj.attr1.method()
+        self.null_obj.attr1.method(1, 2, "abc")
+        
+        self.null_obj.attr1 = "value"
+        self.assertFalse(self.null_obj.attr1)
+        self.null_obj.attr1.attr2 = "value"
+        self.assertFalse(self.null_obj.attr1.attr2)
+        
+        self.assertFalse(self.null_obj)
+    
+    def test_length(self):
+        self.assertEqual(len(self.null_obj), 0)
+    
+    def test_iteration(self):
+        for item in self.null_obj:
+            self.fail("There should be no items in Null object.")
+    
+    def test_equality(self):
+        self.assertEqual(self.null_obj, self.null_obj)
+        self.assertEqual(self.null_obj, utils.Null())
+        
+        self.assertNotEqual(self.null_obj, 1)
+        self.assertNotEqual(self.null_obj, "string")
+        self.assertNotEqual(self.null_obj, object())
+    
+    def test_tests(self):
+        self.assertFalse(self.null_obj)
+        self.assertIsNotNone(self.null_obj)
+    
+    def test_hash(self):
+        a = utils.Null()
+        b = utils.Null()
+        d = {}
+        d[a] = "value"
+        
+        self.assertIn(a, d)
+        self.assertIn(b, d)
+
+    def test_string(self):
+        n = utils.Null(1, 2, kwrd = "abc")
+
+        self.assertEqual(repr(self.null_obj), "Null()")
+        self.assertEqual(repr(n), "Null()")
+
+        self.assertEqual(str(self.null_obj), "")
+        self.assertEqual(str(n), "")
 
 
--- src/nmapps/utils.py
+++ src/nmapps/utils.py
@@ -166,6 +166,11 @@ class Null(object):
         return False
     
     def next(self):
+        """
+        Implements the `iterator protocol
+        <http://docs.python.org/library/stdtypes.html#iterator-types>`_.
+        Iterating over a :class:`Null` object always yields no elements.
+        """
         raise StopIteration
     
     # comparsion
@@ -196,44 +201,7 @@ class Null(object):
     
     def __str__(self):
         "Convert to a string and return it."
-        return "Null"
-
-
-def test():
-    "Perform some decent tests, or rather: demos."
-    
-    # constructing and calling
-    
-    n = Null()
-    n = Null('value')
-    n = Null('value', param='value')
-    
-    n()
-    n('value')
-    n('value', param='value')
-    
-    # attribute handling
-    
-    n.attr1
-    n.attr1.attr2
-    n.method1()
-    n.method1().method2()
-    n.method('value')
-    n.method(param='value')
-    n.method('value', param='value')
-    n.attr1.method1()
-    n.method1().attr1
-    
-    n.attr1 = 'value'
-    n.attr1.attr2 = 'value'
-    
-    del n.attr1
-    del n.attr1.attr2.attr3
-    
-    # representation and conversion to a string
-    
-    assert repr(n) == '<Null>'
-    assert str(n) == 'Null'
+        return ""
 
 
 if __name__ == '__main__':
